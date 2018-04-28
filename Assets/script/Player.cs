52c99e513109362f646f32fa93aab7d42034bd1a
Kuei hua lee <lmcyayaya@gmail.com>
2018-04-28 13:18:37 +0800

    再整合一次

---
+++ Assets/script/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float walk_speed = 100.0f;
+    bool b_spawn;
+    float horizontalDirection;
+    public float maxSpeedX = 10.0f;
+    float maxSpeedY;
+    public float speedX;
+    public float speedY;
+    [Range(400, 600)]
+    public float xForce;
+    [Range(0, 200)]
+    public float yForce;
+    Rigidbody2D rb;
+    [Range(0, 0.5f)]
+    [Header("感應地板距離")]
+    public float distance;
+    [Header("偵測地板射線起點")]
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public bool grounded;
+    bool Isground
+    {
+        get
+        {
+            Vector2 start = groundCheck.position;
+            Vector2 end = new Vector2(start.x, start.y - distance);
+            Debug.DrawLine(start, end, Color.blue);
+            grounded = Physics2D.Linecast(start, end, groundLayer);
+            return grounded;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void jump()
+    {
+        if (Isground && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * yForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void MovementX()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            b_spawn = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            b_spawn = false;
+        }
+        horizontalDirection = Input.GetAxis("Horizontal");
+        if (b_spawn == false)
+        {
+            rb.AddForce(new Vector2(horizontalDirection * xForce * Time.deltaTime, 0));
+        }
+    }
+    public void ControlSpeed()
+    {
+        speedX = rb.velocity.x;
+        speedY = rb.velocity.y;
+        float newspeedX = Mathf.Clamp(speedX, -maxSpeedX, maxSpeedX);
+        rb.velocity = new Vector2(newspeedX, speedY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //move control
+        MovementX();
+        ControlSpeed();
+        jump();
+
+
+    }
+}
--- Assets/script/spawn.cs
+++ Assets/script/spawn.cs
@@ -3,170 +3,364 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class child{
-	public child (string s,Transform t){
-		direction = s;
-		spawn_position = t;
-	}
-	public string direction {get;set;}
-	public Transform spawn_position {get;set;}
+public class child
+{
+    public child(string s, Transform t)
+    {
+        direction = s;
+        spawn_position = t;
+    }
+    public string direction { get; set; }
+    public Transform spawn_position { get; set; }
 }
 
-public class spawn : MonoBehaviour {
-	public Transform prefab;
-	bool is_spawning;
-	public bool cube_exist = false;
-	List <child> history;
-	public GameObject[] gen;
-	void Start () {
-		is_spawning = false;
-		history = new List<child>();
-		history.Add(new child("empty",null));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-			if(Input.GetKeyDown(KeyCode.UpArrow) && is_spawning == false)
-				{
-					is_spawning = true;
-					Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
-					StartCoroutine( Move(child , "up"));
-					Move_self("up");
-					if (cube_exist == false){
-						record2history("up",child);
-					}
-					
-				}
-			if(Input.GetKeyDown(KeyCode.DownArrow) && is_spawning == false)
-				{	
-					is_spawning = true;
-					Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
-					StartCoroutine( Move(child , "down"));
-					Move_self("down");
-					if (cube_exist == false){
-						record2history("down",child);
-					}
-				}
-			if(Input.GetKeyDown(KeyCode.LeftArrow) && is_spawning == false)
-			{
-				is_spawning = true;
-				Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);	
-				StartCoroutine( Move(child , "left"));
-				Move_self("left");
-				if (cube_exist == false){
-					record2history("left",child);
-				}
-			}
-			if(Input.GetKeyDown(KeyCode.RightArrow) && is_spawning == false)
-			{	
-				is_spawning = true;
-				Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
-				StartCoroutine( Move(child , "right"));
-				Move_self("right");
-				if (cube_exist == false){
-					record2history("right",child);
-				}
-			}
-		
-		
-		if(Input.GetKeyDown(KeyCode.F)){
-			gen = GameObject.FindGameObjectsWithTag("generated");
-			GameObject group = new GameObject("blocks");
-			for (int i = 0; i < gen.Length; i++)
-			{
-				gen[i].transform.parent = group.transform;
-			}
+public class spawn : MonoBehaviour
+{
+    public Transform prefab;
+    bool is_spawning;
+    public bool cube_exist = false;
+    List<child> history;
+    public GameObject[] gen;
+    public Transform player;
+    public LayerMask cubeLayer;
+    public bool right;
+    public bool left;
+    public bool up;
+    public bool down;
+    public bool test;
+    [Range(0, 2.0f)]
+    [Header("感應四周距離")]
+    public float distance;
+    [Header("偵測四周射線起點")]
+    public Transform upCubeCheck;
+    public Transform downCubeCheck;
+    public Transform leftCubeCheck;
+    public Transform rightCubeCheck;
+    bool leftCheck
+    {
+        get
+        {
+            Vector2 start = leftCubeCheck.position;
+            Vector2 end = new Vector2(start.x - distance, start.y);
+            Debug.DrawLine(start, end, Color.blue);
+            left = Physics2D.Linecast(start, end, cubeLayer);
+            return left;
+        }
+    }
+    bool rightCheck
+    {
+        get
+        {
+            Vector2 start = rightCubeCheck.position;
+            Vector2 end = new Vector2(start.x + distance, start.y);
+            Debug.DrawLine(start, end, Color.blue);
+            right = Physics2D.Linecast(start, end, cubeLayer);
+            return right;
+        }
+    }
+    bool upCheck
+    {
+        get
+        {
+            Vector2 start = upCubeCheck.position;
+            Vector2 end = new Vector2(start.x, start.y + distance);
+            Debug.DrawLine(start, end, Color.blue);
+            up = Physics2D.Linecast(start, end, cubeLayer);
+            return up;
+        }
+    }
+    bool downCheck
+    {
+        get
+        {
+            Vector2 start = downCubeCheck.position;
+            Vector2 end = new Vector2(start.x, start.y - distance);
+            Debug.DrawLine(start, end, Color.blue);
+            down = Physics2D.Linecast(start, end, cubeLayer);
+            return down;
+        }
+    }
+
+
+
+
+
+
+
+
+
+    void Start()
+    {
+        is_spawning = false;
+        history = new List<child>();
+        history.Add(new child("empty", null));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test = upCheck;
+        test = downCheck;
+        test = leftCheck;
+        test = rightCheck;
+        Spawncube();
+
+
+
+
+    }
+    void Spawncube()
+    {
+
+
+
+        //伸出方塊
+        if (Input.GetKey(KeyCode.Z))
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && is_spawning == false && upCheck == false)
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "up"));
+                Move_self("up");
+                if (cube_exist == false)
+                {
+                    record2history("up", child);
+                }
+
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) && is_spawning == false && downCheck == false)
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "down"));
+                Move_self("down");
+                if (cube_exist == false)
+                {
+                    record2history("down", child);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && is_spawning == false && leftCheck == false)
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "left"));
+                Move_self("left");
+                if (cube_exist == false)
+                {
+                    record2history("left", child);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow) && is_spawning == false && rightCheck == false)
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "right"));
+                Move_self("right");
+                if (cube_exist == false)
+                {
+                    record2history("right", child);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //回收方塊
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && is_spawning == false && upCheck && history.ElementAt(history.Count - 1).direction == "down")
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "up"));
+                Move_self("up");
+                if (cube_exist == false)
+                {
+                    record2history("up", child);
+                }
+
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) && is_spawning == false && downCheck && history.ElementAt(history.Count - 1).direction == "up")
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "down"));
+                Move_self("down");
+                if (cube_exist == false)
+                {
+                    record2history("down", child);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && is_spawning == false && leftCheck && history.ElementAt(history.Count - 1).direction == "right")
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "left"));
+                Move_self("left");
+                if (cube_exist == false)
+                {
+                    record2history("left", child);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow) && is_spawning == false && rightCheck && history.ElementAt(history.Count - 1).direction == "left")
+            {
+                is_spawning = true;
+                Transform child = Instantiate(prefab, this.transform.position, this.transform.rotation);
+                child.transform.parent = player;
+                StartCoroutine(Move(child, "right"));
+                Move_self("right");
+                if (cube_exist == false)
+                {
+                    record2history("right", child);
+                }
+            }
+
+
+
+
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            gen = GameObject.FindGameObjectsWithTag("generated");
+            GameObject group = new GameObject("blocks");
+            for (int i = 0; i < gen.Length; i++)
+            {
+                gen[i].transform.parent = group.transform;
+            }
             group.AddComponent<Rigidbody2D>();
-			transform.position = transform.parent.position;
-		}
-	}
+            group.transform.Translate(new Vector2(1.35f, 0), Space.World);
+            transform.position = transform.parent.position;
+        }
 
-	void record2history(string direction, Transform child_position){
-		history.Add(new child(direction,child_position));
-	}
+    }
 
-	//move generator after spawning cube
-	void Move_self(string dir){
-		Vector3 position = transform.position;
-		 switch(dir){
+    void record2history(string direction, Transform child_position)
+    {
+        history.Add(new child(direction, child_position));
+    }
+
+    //move generator after spawning cube
+    void Move_self(string dir)
+    {
+        Vector3 position = transform.position;
+        switch (dir)
+        {
             case "up":
                 position.y += 1.35f;
-            break;
+                break;
 
             case "down":
                 position.y -= 1.35f;
-            break;
+                break;
 
             case "left":
                 position.x -= 1.35f;
-            break;
+                break;
 
             case "right":
                 position.x += 1.35f;
-            break;
-        }	
-		transform.position = position;
-	}
+                break;
+        }
+        transform.position = position;
+    }
 
-	//move the spawned cube
-	 IEnumerator Move(Transform T, string I){
+    //move the spawned cube
+    IEnumerator Move(Transform T, string I)
+    {
         float F = 0f;
-		
-		float spawn_speed = 0.09f;
-		
-		int n = history.Count - 1;
+
+        float spawn_speed = 0.09f;
+
+        int n = history.Count - 1;
         Vector3 V3 = Vector3.zero;
-        switch(I){
+        switch (I)
+        {
             case "up":
-                V3 = Vector3.up* spawn_speed;
-				if (n!=0 && history.ElementAt(n).direction == "down") //destroy last cube
-				{ 
-					Destroy(history.ElementAt(n).spawn_position.gameObject);
-					history.RemoveAt(n);
-					cube_exist = true;
-				}
-            break;
+                V3 = Vector3.up * spawn_speed;
+                if (n != 0 && history.ElementAt(n).direction == "down") //destroy last cube
+                {
+                    Destroy(history.ElementAt(n).spawn_position.gameObject);
+                    history.RemoveAt(n);
+                    cube_exist = true;
+                }
+                break;
 
             case "down":
-                V3 = Vector3.down* spawn_speed;
-				if (n!=0 && history.ElementAt(n).direction == "up"){ 
-					Destroy(history.ElementAt(n).spawn_position.gameObject);
-					history.RemoveAt(n);
-					cube_exist = true;
-				}
-            break;
+                V3 = Vector3.down * spawn_speed;
+                if (n != 0 && history.ElementAt(n).direction == "up")
+                {
+                    Destroy(history.ElementAt(n).spawn_position.gameObject);
+                    history.RemoveAt(n);
+                    cube_exist = true;
+                }
+
+
+                break;
 
             case "left":
-                V3 = Vector3.left* spawn_speed;
-				if (n!=0 && history.ElementAt(n).direction == "right"){ 
-					Destroy(history.ElementAt(n).spawn_position.gameObject);
-					history.RemoveAt(n);
-					cube_exist = true;
-				}
-            break;
+                V3 = Vector3.left * spawn_speed;
+                if (n != 0 && history.ElementAt(n).direction == "right")
+                {
+                    Destroy(history.ElementAt(n).spawn_position.gameObject);
+                    history.RemoveAt(n);
+                    cube_exist = true;
+                }
+
+                break;
 
             case "right":
-                V3 = Vector3.right* spawn_speed;
-				if (n!=0 && history.ElementAt(n).direction == "left"){ 
-					Destroy(history.ElementAt(n).spawn_position.gameObject);
-					history.RemoveAt(n);
-					cube_exist = true;
-				}
-            break;
+                V3 = Vector3.right * spawn_speed;
+                if (n != 0 && history.ElementAt(n).direction == "left")
+                {
+                    Destroy(history.ElementAt(n).spawn_position.gameObject);
+                    history.RemoveAt(n);
+                    cube_exist = true;
+                }
+
+                break;
         }
-        while(F<1.35f){
+        while (F < 1.35f)
+        {
             F += spawn_speed;
-            T.position +=  V3 ;
+            T.position += V3;
             yield return new WaitForSeconds(0.01f);
         }
-		T.position = transform.position;
-		is_spawning = false;
+        T.position = transform.position;
+        is_spawning = false;
 
-		if(cube_exist == true){
-			Destroy(T.gameObject);
-			cube_exist = false;
-		}
-	
+        if (cube_exist == true)
+        {
+            Destroy(T.gameObject);
+            cube_exist = false;
+        }
+
     }
+
+
+
+
+
+
 }
