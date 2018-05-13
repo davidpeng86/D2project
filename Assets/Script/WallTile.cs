690139b3d2a0904c04e33e3817daa28fa6cea65c
Kuei Hua Lee <lmcyayaya@gmail.com>
2018-05-14 04:26:21 +0800

    Tilemap重整

---
+++ Assets/Script/WallTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class WallTile : Tile {
+
+    [SerializeField]
+    private Sprite[] wallSprites;
+    [SerializeField]
+    private Sprite preview;
+
+
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        
+    }
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        tileData.sprite = wallSprites[0];   
+    }
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/WallTile")]
+    public static void CreateWallTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Walltile", "New Walltile", "asset", "Save walltile", "Assets");
+        if (path == "")
+        {
+            return;
+        }
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<WallTile>(), path);
+    }
+#endif 
+}
