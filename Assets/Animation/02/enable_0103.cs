126116136ae96eda248c2a55e33804bfe4662d40
Wolvz <peng86david@gmail.com>
2018-11-22 09:56:06 +0800

    made basic stage select animation without return keys

---
+++ Assets/Animation/02/enable_0103.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enable_0103 : MonoBehaviour {
+
+	public GameObject stage_01;
+	public GameObject stage_03;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void enable(){
+		stage_01.SetActive(true);
+		stage_03.SetActive(true);
+	}
+
+}
---
+++ Assets/Animation/clickAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickAnimation : MonoBehaviour {
+
+	public GameObject exitButton;
+
+	public Animator button01;
+	public Animator button02;
+	public Animator button03;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void button01Clicked(){
+		button01.SetBool("01_clicked",true);
+		button02.SetBool("01_clicked",true);
+		button03.SetBool("01_clicked",true);
+		exitButton.SetActive(true);
+	}
+
+	public void button02Clicked(){
+		button01.SetBool("02_clicked",true);
+		button02.SetBool("02_clicked",true);
+		button03.SetBool("02_clicked",true);
+		exitButton.SetActive(true);
+	}
+
+	public void button03Clicked(){
+		button01.SetBool("03_clicked",true);
+		button02.SetBool("03_clicked",true);
+		button03.SetBool("03_clicked",true);
+		exitButton.SetActive(true);
+	}
+}
