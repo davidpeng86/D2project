9a2afddf594c26dea5db7d2c086a40467dea98ef
Wolvz <peng86david@gmail.com>
2018-11-28 15:19:45 +0800

    commit be4 showing

---
+++ Assets/pauseMenu/pause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour {
+	public bool ppActive;
+	public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		ppActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void pp(){
+		ppActive = !ppActive;
+		if(ppActive == true){
+			menu.SetActive(true);
+		}
+		else{
+			menu.SetActive(false);
+		}
+		
+
+	}
+}
