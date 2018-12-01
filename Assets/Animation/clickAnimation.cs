856c1ffd3cc9d076c88b511937bfd20838b13c9d
Wolvz <peng86david@gmail.com>
2018-12-02 00:44:22 +0800

    實現關卡選擇功能

--- Assets/Animation/clickAnimation.cs
+++ Assets/Animation/clickAnimation.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class clickAnimation : MonoBehaviour {
-
+	public sceneManager scene;
 	public GameObject exitButton;
 
+	public int clickCount = 1;
 	public Animator button01;
 	public Animator button02;
 	public Animator button03;
@@ -22,33 +23,53 @@ public class clickAnimation : MonoBehaviour {
 	}
 
 	public void button01Clicked(){
-		button01.SetBool("01_clicked",true);
-		button02.SetBool("01_clicked",true);
-		button03.SetBool("01_clicked",true);
+		if(clickCount == 1){
+			button01.SetBool("01_clicked",true);
+			button02.SetBool("01_clicked",true);
+			button03.SetBool("01_clicked",true);
 
-		animationName = "01_clicked";
-		exitButton.SetActive(true);
+			animationName = "01_clicked";
+			exitButton.SetActive(true);
+			clickCount += 1;
+		}
+		else if(clickCount == 2){
+			scene.sceneChange("Tutorial");
+		}
+
 	}
 
 	public void button02Clicked(){
-		button01.SetBool("02_clicked",true);
-		button02.SetBool("02_clicked",true);
-		button03.SetBool("02_clicked",true);
+		if(clickCount == 1){
+			button01.SetBool("02_clicked",true);
+			button02.SetBool("02_clicked",true);
+			button03.SetBool("02_clicked",true);
 
-		animationName = "02_clicked";
-		exitButton.SetActive(true);
+			animationName = "02_clicked";
+			exitButton.SetActive(true);
+			clickCount += 1;
+		}
+		else if(clickCount == 2){
+			scene.sceneChange("Stage1");
+		}
 	}
 
 	public void button03Clicked(){
-		button01.SetBool("03_clicked",true);
-		button02.SetBool("03_clicked",true);
-		button03.SetBool("03_clicked",true);
+		if(clickCount == 1){
+			button01.SetBool("03_clicked",true);
+			button02.SetBool("03_clicked",true);
+			button03.SetBool("03_clicked",true);
 
-		animationName = "03_clicked";
-		exitButton.SetActive(true);
+			animationName = "03_clicked";
+			exitButton.SetActive(true);
+			clickCount += 1;
+		}
+		else if(clickCount == 2){
+			scene.sceneChange("Stage2");
+		}
 	}
 
 	public void leave(){
+		clickCount = 1;
 		button01.SetBool(animationName,false);
 		button02.SetBool(animationName,false);
 		button03.SetBool(animationName,false);
