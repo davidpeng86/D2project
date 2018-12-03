2a2f4b451df054ec1b7d584509ace39ffdd853e0
Kuei Hua Lee <lmcyayaya@gmail.com>
2018-12-03 21:25:33 +0800

    Finsh Point完成

--- Assets/Script/Map/FinishPoint.cs
+++ Assets/Script/Map/FinishPoint.cs
@@ -8,23 +8,47 @@ public class FinishPoint : MonoBehaviour {
 	public GameObject Up;
 	private Vector3 Save;
 	private Vector2 velocity;
+	private bool In;
 	// Use this for initialization
 	void Start () {
 		Save = Up.transform.position;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(In)
+		{
+			if(Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				endMenu.SetActive(true);
+				s_database.Save();
+			}
+			Up.transform.position = new Vector3(Up.transform.position.x,Mathf.SmoothDamp(Up.transform.position.y,Save.y+2.56f,ref velocity.y,0.2f),Up.transform.position.z);
+		}
+		else
+		{
+			Up.transform.position = new Vector3(Up.transform.position.x,Mathf.SmoothDamp(Up.transform.position.y,Save.y,ref velocity.y,0.2f),Up.transform.position.z);
+			if(Up.transform.position.y <= Save.y+0.02f)
+			{
+				Up.SetActive(false);
+			}
+		}
+
 		
 	}
 	private void OnTriggerEnter2D(Collider2D col)
 	{
 		if(col.tag == "Player")
 		{
-			Up.transform.position = new Vector3(transform.position.x,Mathf.SmoothDamp(transform.position.y,Save.y+2.56f,ref velocity.y,0.2f),transform.position.z);
-			endMenu.SetActive(true);
-			s_database.Save();
+			Up.SetActive(true);
+			In = true;
+		}
+	}
+	private void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.tag == "Player")
+		{
+			In = false;
 		}
 	}
 }
